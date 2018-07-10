076e9846823a36e36cc2a3ddd020287a0cc513f0
FionnMK <41059531+FionnMK@users.noreply.github.com>
2018-07-10 16:19:11 +0100

    Update main.tf

--- delete_team/main.tf
+++ delete_team/main.tf
@@ -1,4 +1,6 @@
-terraform destroy -target resource_group_common."$(var.workspace)"
 variable "workspace" {
   description = "Name of the workspace ie. oea"
 }
+
+
+terraform destroy -target resource_group_common."$(var.workspace)"
